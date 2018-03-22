7e7b1177b6eb880b11adf87370d8958b3b65a359
Jorge B Nunez <jjbnunez97@gmail.com>
2018-03-22 19:04:42 -0400

    Further Defined Module
    Added further functionality according to character device driver examples. Taking a short mental break from this for now.

--- char-kmod-jbn.c
+++ char-kmod-jbn.c
@@ -8,25 +8,35 @@
 
 
 #include <linux/module.h>	// Core header for modules.
+#include <linux/device.h>	// Supports driver model.
 #include <linux/kernel.h>	// Kernel header for convenient functions.
+#include <linux/fs.h>		// File-system support.
+#include <linux/uaccess.h>	// User access copy function support.
+#define DEVICE_NAME "char-kmod"	// Device name.
+#define CLASS_NAME "char-dev"	// Class name. This is a character device.
 
 
 /**
- * Module information.
+ * Mod Info:	The module uses a GPL to avoid tainting the kernel.
  */
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Arati Banerjee, Huong Dang, and Jorge B. Nunez");
 
 
-static char *string = "boiyyyy";
+static int  major_number;
+static char data[1024] = {'\0'};
+static int  data_size;
 
 
-int init_module(void) {
-	printk(KERN_INFO "Installing module. Whaddap %s!!!\n", string);
+int init_module(void)
+{
+	printk(KERN_INFO "Char-KMod: Installing module.\n");
 	return 0;
 }
 
 
-void cleanup_module(void) {
-	printk(KERN_INFO "Removing module. Peace out %s!!!\n", string);
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Removing module.\n");
+	return;
 }
