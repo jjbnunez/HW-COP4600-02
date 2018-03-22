529ca71ab94ec822c1f0871be50520b7be1088af
Jorge B Nunez <jjbnunez97@gmail.com>
2018-03-22 14:26:11 -0400

    Progress Modification
    Learning about kernel-space restrictions and such. Next step is learning about kernel parameters.

--- char-kmod-jbn.c
+++ char-kmod-jbn.c
@@ -1,16 +1,32 @@
-// Jorge Nunez
+/**
+ * File:	char-kmod-jbn.c
+ * Author:	Jorge Brandon Nunez
+ * Class:	COP4600-SP18
+ * Professor:	Dr. Gerber
+ * Due Date:	2018-03-23
+ */
 
 
-#include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/module.h>	// Core header for modules.
+#include <linux/kernel.h>	// Kernel header for convenient functions.
+
+
+/**
+ * Module information.
+ */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Arati Banerjee, Huong Dang, and Jorge B. Nunez");
+
+
+static char *string = "boiyyyy";
 
 
 int init_module(void) {
-  printk(KERN_INFO "Installing module.\n");
-  return 0;
+	printk(KERN_INFO "Installing module. Whaddap %s!!!\n", string);
+	return 0;
 }
 
 
 void cleanup_module(void) {
-  printk(KERN_INFO "Removing module.\n");
+	printk(KERN_INFO "Removing module. Peace out %s!!!\n", string);
 }
